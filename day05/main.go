494281c1708370f11684282065dd0c16901df22b
Alex Mirski-Fitton <alex@amfweb.co.uk>
2018-12-17 17:33:01 +0000

    Day 5 part 2

--- day05/main.go
+++ day05/main.go
@@ -3,13 +3,32 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"regexp"
 )
 
 func main() {
 	polymerBytes, _ := ioutil.ReadFile("input.txt")
 	polymer := string(polymerBytes)
+	// part1(polymer)
+	part2(polymer)
+}
+func part1(polymer string) {
 	polymer = fullyReact(polymer)
 	fmt.Printf("Polymer length: %d\n", len(polymer))
+}
+
+func part2(polymer string) {
+	bestLength := len(polymer)
+	for char := 'a'; char <= 'z'; char++ {
+		re, _ := regexp.Compile(fmt.Sprintf("(?i)%c", char))
+		testPoly := string(re.ReplaceAll([]byte(polymer), []byte{}))
+		testPoly = fullyReact(testPoly)
+		if len(testPoly) < bestLength {
+			bestLength = len(testPoly)
+			fmt.Printf("Better alternative: %c => %d\n", char, len(testPoly))
+		}
+
+	}
 }
 
 func fullyReact(polymer string) string {
