9b35e89c8ff2582b8c012aa0750a247acf44ef0b
Alex Mirski-Fitton <alex@amfweb.co.uk>
2019-12-12 21:34:28 +0000

    Part 2 benchmark

--- day12/main_test.go
+++ day12/main_test.go
@@ -131,8 +131,8 @@ func BenchmarkPart1(b *testing.B) {
 	}
 }
 
-// func BenchmarkPart2(b *testing.B) {
-// 	for i := 0; i < b.N; i++ {
-// 		part2()
-// 	}
-// }
+func BenchmarkPart2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		part2()
+	}
+}
