4f1f973d9e4226996c010752ae07c33c1a9ef106
Alex Mirski-Fitton <alex@amfweb.co.uk>
2019-12-24 07:53:28 +0000

    Day 24 part 2 solved

--- day24/main.go
+++ day24/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"sort"
 
 	"github.com/adsmf/adventofcode2019/utils"
 )
@@ -18,11 +19,15 @@ func part1() int {
 }
 
 func part2() int {
-	return 0
+	p := loadMap("input.txt")
+	return p.iterRecursiveN(200)
 }
 
+type planes map[int]plane
+type plane map[point]tile
+
 type planet struct {
-	grid       map[point]tile
+	grids      planes
 	minX, minY int
 	maxX, maxY int
 }
@@ -40,14 +45,28 @@ func (p *planet) findRepeat() int {
 	}
 }
 
-func (p *planet) iter() {
-	newGrid := map[point]tile{}
+func (p *planet) iterRecursiveN(steps int) int {
+	midX := p.maxX / 2
+	midY := p.maxY / 2
 
-	for pos, t := range p.grid {
+	delete(p.grids[0], point{midX, midY, 0})
+
+	lastBugs := 0
+	for i := 0; i < steps; i++ {
+		lastBugs = p.iterRecursive()
+	}
+
+	return lastBugs
+}
+
+func (p *planet) iter() {
+	newGrid := plane{}
+
+	for pos, t := range p.grids[0] {
 		infested := t.tileType == tileTypeInfested
 		surrounding := 0
 		for _, neighbour := range pos.neighbours() {
-			if p.grid[neighbour].tileType == tileTypeInfested {
+			if p.grids[0][neighbour].tileType == tileTypeInfested {
 				surrounding++
 			}
 		}
@@ -58,12 +77,63 @@ func (p *planet) iter() {
 			newGrid[pos] = tile{tileType: tileTypeEmpty}
 		}
 	}
-	p.grid = newGrid
+	p.grids[0] = newGrid
+}
+
+func (p *planet) iterRecursive() int {
+
+	width := p.maxX - p.minX + 1
+	height := p.maxY - p.minY + 1
+
+	levels := []int{}
+	for level := range p.grids {
+		levels = append(levels, level)
+	}
+	sort.Ints(levels)
+	lowestLevel := levels[0]
+	highestLevel := levels[len(levels)-1]
+	p.set(point{0, 0, lowestLevel - 1}, tile{tileType: tileTypeEmpty})
+	p.set(point{0, 0, highestLevel + 1}, tile{tileType: tileTypeEmpty})
+
+	newGrids := planes{}
+	bugs := 0
+	for level, grid := range p.grids {
+		newGrid := plane{}
+
+		for pos, t := range grid {
+			infested := t.tileType == tileTypeInfested
+			surrounding := 0
+			for _, neighbour := range pos.recursiveNeighbours(width, height) {
+				if p.infested(neighbour) {
+					surrounding++
+				}
+			}
+			if surrounding == 1 ||
+				(!infested && surrounding == 2) {
+				newGrid[pos] = tile{tileType: tileTypeInfested}
+				bugs++
+			} else {
+				newGrid[pos] = tile{tileType: tileTypeEmpty}
+			}
+		}
+		newGrids[level] = newGrid
+
+	}
+	p.grids = newGrids
+	return bugs
 }
 
 func (p *planet) set(pos point, val tile) {
 	val.pos = pos
-	p.grid[pos] = val
+	if p.grids[pos.level] == nil {
+		p.grids[pos.level] = plane{}
+		for pos0 := range p.grids[0] {
+			posNew := pos0
+			posNew.level = pos.level
+			p.grids[posNew.level][posNew] = tile{tileType: tileTypeEmpty, pos: posNew}
+		}
+	}
+	p.grids[pos.level][pos] = val
 	if p.minX > pos.x {
 		p.minX = pos.x
 	}
@@ -77,12 +147,28 @@ func (p *planet) set(pos point, val tile) {
 		p.maxY = pos.y
 	}
 }
+func (p *planet) infested(pos point) bool {
+	if p.grids[pos.level] == nil {
+		return false
+	}
+	return p.grids[pos.level][pos].tileType == tileTypeInfested
+}
 
 func (p planet) String() string {
 	newString := ""
-	for y := p.minY; y <= p.maxY; y++ {
-		for x := p.minX; x <= p.maxX; x++ {
-			newString += fmt.Sprintf("%v", p.grid[point{x, y}])
+	levels := []int{}
+	for level := range p.grids {
+		levels = append(levels, level)
+	}
+	sort.Ints(levels)
+	for _, level := range levels {
+		newString += fmt.Sprintf("Level %d\n", level)
+		grid := p.grids[level]
+		for y := p.minY; y <= p.maxY; y++ {
+			for x := p.minX; x <= p.maxX; x++ {
+				newString += fmt.Sprintf("%v", grid[point{x, y, level}])
+			}
+			newString += fmt.Sprintln()
 		}
 		newString += fmt.Sprintln()
 	}
@@ -91,7 +177,7 @@ func (p planet) String() string {
 
 func (p planet) biodiversity() int {
 	bio := 0
-	for pos, t := range p.grid {
+	for pos, t := range p.grids[0] {
 		if t.tileType == tileTypeInfested {
 			id := pos.x + pos.y*(p.maxX+1)
 			bio += int(math.Pow(2, float64(id)))
@@ -101,16 +187,84 @@ func (p planet) biodiversity() int {
 }
 
 type point struct {
-	x, y int
+	x, y  int
+	level int
 }
 
 func (p point) neighbours() []point {
 	return []point{
-		point{p.x - 1, p.y},
-		point{p.x + 1, p.y},
-		point{p.x, p.y - 1},
-		point{p.x, p.y + 1},
+		point{p.x - 1, p.y, p.level},
+		point{p.x + 1, p.y, p.level},
+		point{p.x, p.y - 1, p.level},
+		point{p.x, p.y + 1, p.level},
 	}
+}
+
+func (p point) String() string {
+	return fmt.Sprintf("{%d, %d, %d}", p.x, p.y, p.level)
+}
+
+func (p point) recursiveNeighbours(w, h int) []point {
+	midX := (w - 1) / 2
+	midY := (h - 1) / 2
+
+	basePoints := []point{
+		point{p.x - 1, p.y, p.level},
+		point{p.x + 1, p.y, p.level},
+		point{p.x, p.y - 1, p.level},
+		point{p.x, p.y + 1, p.level},
+	}
+	realPoints := []point{}
+
+	for _, pos := range basePoints {
+		normal := true
+		// Check adjacency to center
+		if pos.x == midX && pos.y == midY {
+			if p.y == midY+1 {
+				normal = false
+				for x2 := 0; x2 < w; x2++ {
+					realPoints = append(realPoints, point{x2, h - 1, pos.level - 1})
+				}
+			} else if p.y == midY-1 {
+				normal = false
+				for x2 := 0; x2 < w; x2++ {
+					realPoints = append(realPoints, point{x2, 0, pos.level - 1})
+				}
+			} else if p.x == midX+1 {
+				normal = false
+				for y2 := 0; y2 < h; y2++ {
+					realPoints = append(realPoints, point{w - 1, y2, pos.level - 1})
+				}
+			} else if p.x == midX-1 {
+				normal = false
+				for y2 := 0; y2 < h; y2++ {
+					realPoints = append(realPoints, point{0, y2, pos.level - 1})
+				}
+			}
+		}
+
+		// Check adjacency to edge
+		if pos.x == -1 {
+			normal = false
+			realPoints = append(realPoints, point{midX - 1, midY, pos.level + 1})
+		} else if pos.x == w {
+			normal = false
+			realPoints = append(realPoints, point{midX + 1, midY, pos.level + 1})
+		}
+
+		if pos.y == -1 {
+			normal = false
+			realPoints = append(realPoints, point{midX, midY - 1, pos.level + 1})
+		} else if pos.y == h {
+			normal = false
+			realPoints = append(realPoints, point{midX, midY + 1, pos.level + 1})
+		}
+
+		if normal {
+			realPoints = append(realPoints, pos)
+		}
+	}
+	return realPoints
 }
 
 type tile struct {
@@ -141,13 +295,13 @@ const (
 
 func loadMap(filename string) planet {
 	newPlanet := planet{
-		grid: map[point]tile{},
+		grids: planes{},
 	}
 
 	lines := utils.ReadInputLines(filename)
 	for y, line := range lines {
 		for x, char := range line {
-			pos := point{x, y}
+			pos := point{x, y, 0}
 			switch {
 			case char == '.':
 				newPlanet.set(pos, tile{tileType: tileTypeEmpty})
--- day24/main_test.go
+++ day24/main_test.go
@@ -6,6 +6,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestAnswers(t *testing.T) {
+	assert.Equal(t, 11042850, part1())
+	assert.Equal(t, 1967, part2())
+}
+
+func ExampleMain() {
+	main()
+	//Output:
+	//Part 1: 11042850
+	//Part 2: 1967
+}
+
 func TestPart1Examples(t *testing.T) {
 	p := loadMap("examples/ex1.0.txt")
 	t.Logf("%v\n", p)
@@ -22,21 +34,151 @@ func TestPart1Examples(t *testing.T) {
 
 }
 
+func TestPart2Example(t *testing.T) {
+	p := loadMap("examples/ex1.0.txt")
+	p.iterRecursiveN(0)
+	t.Logf("Initial:\n%v", p)
+	result := p.iterRecursiveN(10)
+	t.Logf("After 10:\n%v", p)
+	assert.Equal(t, 99, result)
+}
+
+func TestRecursiveNeighbours19(t *testing.T) {
+	pos := point{3, 3, 0}
+	expected := []point{
+		point{2, 3, 0},
+		point{4, 3, 0},
+		point{3, 2, 0},
+		point{3, 4, 0},
+	}
+	neighbours := pos.recursiveNeighbours(5, 5)
+	t.Logf("Expected:\n%v", expected)
+	t.Logf("Got:\n%v", neighbours)
+	assert.Subset(t, expected, neighbours)
+	assert.Subset(t, neighbours, expected)
+}
+
+func TestRecursiveNeighboursE(t *testing.T) {
+	pos := point{4, 0, 0}
+	expected := []point{
+		point{3, 0, 0},
+		point{3, 2, 1},
+		point{4, 1, 0},
+		point{2, 1, 1},
+	}
+	neighbours := pos.recursiveNeighbours(5, 5)
+	t.Logf("Expected:\n%v", expected)
+	t.Logf("Got:\n%v", neighbours)
+	assert.Subset(t, neighbours, expected)
+	assert.Subset(t, expected, neighbours)
+}
+
+func TestRecursiveNeighboursU(t *testing.T) {
+	pos := point{0, 4, -1}
+	expected := []point{
+		point{1, 2, 0},
+		point{1, 4, -1},
+		point{0, 3, -1},
+		point{2, 3, 0},
+	}
+	neighbours := pos.recursiveNeighbours(5, 5)
+	t.Logf("Expected:\n%v", expected)
+	t.Logf("Got:\n%v", neighbours)
+	assert.Subset(t, neighbours, expected)
+	assert.Subset(t, expected, neighbours)
+}
+
+func TestRecursiveNeighbours14(t *testing.T) {
+	pos := point{3, 2, 0}
+	expected := []point{
+		// Same level
+		point{3, 1, 0},
+		point{3, 3, 0},
+		point{4, 2, 0},
+		// Inner level
+		point{4, 0, -1},
+		point{4, 1, -1},
+		point{4, 2, -1},
+		point{4, 3, -1},
+		point{4, 4, -1},
+	}
+	neighbours := pos.recursiveNeighbours(5, 5)
+	t.Logf("Expected:\n%v", expected)
+	t.Logf("Got:\n%v", neighbours)
+	assert.Len(t, neighbours, 8)
+	assert.Subset(t, neighbours, expected)
+	assert.Subset(t, expected, neighbours)
+}
+
+func TestRecursiveNeighbours8(t *testing.T) {
+	pos := point{2, 1, 0}
+	expected := []point{
+		// Same level
+		point{2, 0, 0},
+		point{1, 1, 0},
+		point{3, 1, 0},
+		// Inner level
+		point{0, 0, -1},
+		point{1, 0, -1},
+		point{2, 0, -1},
+		point{3, 0, -1},
+		point{4, 0, -1},
+	}
+	neighbours := pos.recursiveNeighbours(5, 5)
+	t.Logf("Expected:\n%v", expected)
+	t.Logf("Got:\n%v", neighbours)
+	assert.Len(t, neighbours, 8)
+	assert.Subset(t, neighbours, expected)
+	assert.Subset(t, expected, neighbours)
+}
+
+func TestRecursiveNeighbours18(t *testing.T) {
+	pos := point{2, 3, 0}
+	expected := []point{
+		// Same level
+		point{2, 4, 0},
+		point{1, 3, 0},
+		point{3, 3, 0},
+		// Inner level
+		point{0, 4, -1},
+		point{1, 4, -1},
+		point{2, 4, -1},
+		point{3, 4, -1},
+		point{4, 4, -1},
+	}
+	neighbours := pos.recursiveNeighbours(5, 5)
+	t.Logf("Expected:\n%v", expected)
+	t.Logf("Got:\n%v", neighbours)
+	assert.Len(t, neighbours, 8)
+	assert.Subset(t, neighbours, expected)
+	assert.Subset(t, expected, neighbours)
+}
+
+func TestRecursiveNeighboursL(t *testing.T) {
+	pos := point{1, 2, -1}
+	expected := []point{
+		// Same level
+		point{1, 1, -1},
+		point{1, 3, -1},
+		point{0, 2, -1},
+		// Inner level
+		point{0, 0, -2},
+		point{0, 1, -2},
+		point{0, 2, -2},
+		point{0, 3, -2},
+		point{0, 4, -2},
+	}
+	neighbours := pos.recursiveNeighbours(5, 5)
+	t.Logf("Expected:\n%v", expected)
+	t.Logf("Got:\n%v", neighbours)
+	assert.Len(t, neighbours, 8)
+	assert.Subset(t, neighbours, expected)
+	assert.Subset(t, expected, neighbours)
+}
+
 func TestBiodiversity(t *testing.T) {
 	p := loadMap("examples/biodiversity.txt")
 	assert.Equal(t, 2129920, p.biodiversity())
-}
-
-func TestAnswers(t *testing.T) {
-	assert.Equal(t, 11042850, part1())
-	assert.Equal(t, 0, part2())
-}
-
-func ExampleMain() {
-	main()
-	//Output:
-	//Part 1: 11042850
-	//Part 2: 0
 }
 
 func BenchmarkPart1(b *testing.B) {
