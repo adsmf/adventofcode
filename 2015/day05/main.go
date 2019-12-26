3bb09917e11b4619b15fbbc7b2ff297bfefed4a0
Alex Mirski-Fitton <alex@amfweb.co.uk>
2019-12-26 22:14:09 +0000

    Day 5 solved

---
+++ 2015/day05/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"github.com/adsmf/adventofcode/utils"
+)
+
+func main() {
+	fmt.Printf("Part 1: %d\n", part1())
+	fmt.Printf("Part 2: %d\n", part2())
+}
+
+func part1() int {
+	return countNice("input.txt")
+}
+
+func part2() int {
+	return countNice2("input.txt")
+}
+
+func countNice(filename string) int {
+	lines := utils.ReadInputLines(filename)
+	nice := 0
+	for _, line := range lines {
+		if niceString(line) {
+			nice++
+		}
+	}
+	return nice
+}
+
+func niceString(input string) bool {
+	vowels := 0
+	var lastChar rune
+	double := false
+	for _, char := range input {
+		switch char {
+		case 'a', 'e', 'i', 'o', 'u':
+			vowels++
+		case 'b', 'd', 'q', 'y':
+			if lastChar == char-rune(1) {
+				return false
+			}
+		}
+		if lastChar == char {
+			double = true
+		}
+		lastChar = char
+	}
+	return double && vowels >= 3
+}
+
+func countNice2(filename string) int {
+	lines := utils.ReadInputLines(filename)
+	nice := 0
+	for _, line := range lines {
+		if niceString2(line) {
+			nice++
+		}
+	}
+	return nice
+}
+
+func niceString2(input string) bool {
+	pairs := map[string]int{}
+	// r2string := ""
+	rule2 := false
+	for i := 1; i < len(input); i++ {
+		pair := input[i-1 : i+1]
+		pairs[pair]++
+		var p1, p2, p3 byte
+		cur := input[i]
+		p1 = input[i-1]
+		if i > 1 {
+			p2 = input[i-2]
+			if i > 2 {
+				p3 = input[i-3]
+			}
+		}
+		if cur == p2 {
+			rule2 = true
+		}
+		if cur == p1 && cur == p2 {
+			if p3 != cur {
+				pairs[pair]--
+			}
+		}
+	}
+	rule1 := false
+	for _, count := range pairs {
+		if count > 1 {
+			rule1 = true
+			break
+		}
+	}
+	return rule1 && rule2
+}
---
+++ 2015/day05/main_test.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPart1Examples(t *testing.T) {
+	type testDef struct {
+		// Test structure here
+	}
+	tests := []testDef{
+		// Test data here
+	}
+	for id, test := range tests {
+		t.Run(fmt.Sprintf("Part1-Test%d", id+1), func(t *testing.T) {
+			t.Logf("Test def:\n %v", test)
+			// Assertions here
+		})
+	}
+}
+
+func TestPart2Examples(t *testing.T) {
+	type testDef struct {
+		input string
+		nice  bool
+	}
+	tests := []testDef{
+		testDef{"qjhvhtzxzqqjkmpb", true},
+		testDef{"xxyxx", true},
+		testDef{"uurcxstgmygtbstg", false},
+		testDef{"ieodomkazucvgmuy", false},
+		testDef{"xxyzxx", false},
+		testDef{"xxyzxxodo", true},
+		testDef{"odoxxyzxx", true},
+		testDef{"aaaodo", false},
+	}
+	for id, test := range tests {
+		t.Run(fmt.Sprintf("Part1-Test%d", id+1), func(t *testing.T) {
+			t.Logf("Test def:\n %#v", test)
+			assert.Equal(t, test.nice, niceString2(test.input))
+		})
+	}
+}
+
+func TestAnswers(t *testing.T) {
+	assert.Equal(t, 255, part1())
+	assert.Equal(t, 55, part2())
+}
+
+func ExampleMain() {
+	main()
+	//Output:
+	//Part 1: 255
+	//Part 2: 55
+}
+
+func BenchmarkPart1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		part1()
+	}
+}
+
+func BenchmarkPart2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		part2()
+	}
+}
