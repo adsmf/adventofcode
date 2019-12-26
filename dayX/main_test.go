dfacabd7d7ab4c943913eed764f2aa63f74b4d01
Alex Mirski-Fitton <alex@amfweb.co.uk>
2019-12-26 21:04:19 +0000

    Add more test boilerplate

--- dayX/main_test.go
+++ dayX/main_test.go
@@ -1,17 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestPart1Examples(t *testing.T) {
-
+	type testDef struct {
+		// Test structure here
+	}
+	tests := []testDef{
+		// Test data here
+	}
+	for id, test := range tests {
+		t.Run(fmt.Sprintf("Part1-Test%d", id+1), func(t *testing.T) {
+			t.Logf("Test def:\n %v", test)
+			// Assertions here
+		})
+	}
 }
 
 func TestPart2Examples(t *testing.T) {
-
+	type testDef struct {
+		// Test structure here
+	}
+	tests := []testDef{
+		// Test data here
+	}
+	for id, test := range tests {
+		t.Run(fmt.Sprintf("Part1-Test%d", id+1), func(t *testing.T) {
+			t.Logf("Test def:\n %v", test)
+			// Assertions here
+		})
+	}
 }
 
 func TestAnswers(t *testing.T) {
