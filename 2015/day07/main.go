de498f25951fe1542acdc88a5c7fca4996751bac
Alex Mirski-Fitton <alex@amfweb.co.uk>
2019-12-27 09:20:30 +0000

    Day 7 part 1 solved

---
+++ 2015/day07/main.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/adsmf/adventofcode/utils"
+)
+
+func main() {
+	fmt.Printf("Part 1: %d\n", part1())
+	fmt.Printf("Part 2: %d\n", part2())
+}
+
+func part1() int {
+	w := loadCircuit("input.txt")
+	s := w.run()
+	return int(s["a"])
+}
+
+func part2() int {
+	return -1
+}
+
+type signals map[string]uint16
+type wiring map[string]string
+
+func (w wiring) run() signals {
+	s := signals{}
+	for len(w) > 0 {
+		foundSignals := []string{}
+		for res, inp := range w {
+			num, err := strconv.Atoi(inp)
+			if err == nil {
+				s[res] = uint16(num)
+				foundSignals = append(foundSignals, res)
+				continue
+			}
+			if strings.HasPrefix(inp, "NOT ") {
+				notOf := inp[4:]
+				if val, found := decodeParam(notOf, s); found {
+					s[res] = -val - 1
+					foundSignals = append(foundSignals, res)
+					continue
+				}
+			}
+			if strings.Contains(inp, " AND ") {
+				sigs := strings.Split(inp, " AND ")
+				valA, foundA := decodeParam(sigs[0], s)
+				valB, foundB := decodeParam(sigs[1], s)
+				if foundA && foundB {
+					s[res] = valA & valB
+					foundSignals = append(foundSignals, res)
+					continue
+				}
+			}
+			if strings.Contains(inp, " OR ") {
+				sigs := strings.Split(inp, " OR ")
+				valA, foundA := decodeParam(sigs[0], s)
+				valB, foundB := decodeParam(sigs[1], s)
+				if foundA && foundB {
+					s[res] = valA | valB
+					foundSignals = append(foundSignals, res)
+					continue
+				}
+			}
+
+			if strings.Contains(inp, " LSHIFT ") {
+				sigs := strings.Split(inp, " LSHIFT ")
+				valA, foundA := decodeParam(sigs[0], s)
+				valB, foundB := decodeParam(sigs[1], s)
+				if foundA && foundB {
+					s[res] = valA << valB
+					foundSignals = append(foundSignals, res)
+					continue
+				}
+			}
+			if strings.Contains(inp, " RSHIFT ") {
+				sigs := strings.Split(inp, " RSHIFT ")
+				valA, foundA := decodeParam(sigs[0], s)
+				valB, foundB := decodeParam(sigs[1], s)
+				if foundA && foundB {
+					s[res] = valA >> valB
+					foundSignals = append(foundSignals, res)
+					continue
+				}
+			}
+			if val, found := decodeParam(inp, s); found {
+				s[res] = val
+				foundSignals = append(foundSignals, res)
+				continue
+			}
+		}
+		if len(foundSignals) == 0 {
+			fmt.Printf("Couldn't find anything else\n%v\n", w)
+			break
+		}
+		for _, f := range foundSignals {
+			delete(w, f)
+		}
+	}
+	return s
+}
+
+func decodeParam(inp string, s signals) (uint16, bool) {
+	if sig, found := s[inp]; found {
+		return sig, true
+	}
+	num, err := strconv.Atoi(inp)
+	if err == nil {
+		return uint16(num), true
+	}
+	return 0, false
+}
+
+func loadCircuit(filename string) wiring {
+	w := wiring{}
+
+	lines := utils.ReadInputLines(filename)
+	for _, line := range lines {
+		if !strings.Contains(line, " -> ") {
+			continue
+		}
+		parts := strings.Split(line, " -> ")
+		w[parts[1]] = parts[0]
+	}
+
+	return w
+}
---
+++ 2015/day07/main_test.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPart1Examples(t *testing.T) {
+	type testDef struct {
+		input  string
+		output signals
+	}
+	tests := []testDef{
+		testDef{
+			input: "examples/ex1.txt",
+			output: signals{
+				"d": 72,
+				"e": 507,
+				"f": 492,
+				"g": 114,
+				"h": 65412,
+				"i": 65079,
+				"x": 123,
+				"y": 456,
+			},
+		},
+	}
+	for id, test := range tests {
+		t.Run(fmt.Sprintf("Part1-Test%d", id+1), func(t *testing.T) {
+			t.Logf("Test def:\n %v", test)
+			w := loadCircuit(test.input)
+			s := w.run()
+			assert.Equal(t, test.output, s)
+			// Assertions here
+		})
+	}
+}
+
+func TestPart2Examples(t *testing.T) {
+	type testDef struct {
+		// Test structure here
+	}
+	tests := []testDef{
+		// Test data here
+	}
+	for id, test := range tests {
+		t.Run(fmt.Sprintf("Part1-Test%d", id+1), func(t *testing.T) {
+			t.Logf("Test def:\n %v", test)
+			// Assertions here
+		})
+	}
+}
+
+func TestAnswers(t *testing.T) {
+}
+
+func ExampleMain() {
+	main()
+	//Output:
+	//Part 1: 3176
+	//Part 2: -1
+}
+
+func BenchmarkPart1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		part1()
+	}
+}
+
+func BenchmarkPart2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		part2()
+	}
+}
