afac712d27f584e42ef2b4361ec98b40123fe294
Alex Mirski-Fitton <alex@amfweb.co.uk>
2019-12-31 18:00:13 +0000

    Day 20 solved

---
+++ 2015/day20/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+)
+
+var input = 33100000
+
+func main() {
+	fmt.Printf("Part 1: %d\n", part1())
+	fmt.Printf("Part 2: %d\n", part2())
+}
+
+func part1() int {
+	return equalDeliveries()
+}
+
+func part2() int {
+	return equalDeliveries2()
+}
+
+func equalDeliveries() int {
+	deliveries := make(map[int]int, input)
+
+	for elf := 1; elf <= input/10; elf++ {
+		for house := elf; house <= input/10; house += elf {
+			deliveries[house] += elf * 10
+		}
+	}
+
+	for house := 1; house <= input; house++ {
+		if deliveries[house] >= input {
+			return house
+		}
+	}
+	return -1
+}
+
+func equalDeliveries2() int {
+	deliveries := make(map[int]int, input)
+
+	for elf := 1; elf <= input/10; elf++ {
+		for house := elf; house <= input/10 && house <= elf*50; house += elf {
+			deliveries[house] += elf * 11
+		}
+	}
+
+	for house := 1; house <= input; house++ {
+		if deliveries[house] >= input {
+			return house
+		}
+	}
+	return -1
+}
