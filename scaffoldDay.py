ac3365695182b98114aa2f495accb5fe2984bbc3
Alex Mirski-Fitton <alex@amfweb.co.uk>
2020-12-01 22:45:07 +0000

    Scaffold script cleanup

--- scaffoldDay.py
+++ scaffoldDay.py
@@ -3,6 +3,43 @@ import os
 import requests
 import shutil
 import sys
+import webbrowser
+
+def main():
+  now = datetime.datetime.now()
+
+  if now.month != 12:
+    sys.exit("Wrong month")
+  
+  year = now.year
+  day = now.day
+  
+  if day>25:
+    sys.exit("Come back next year")
+  
+  basePath = os.path.dirname(os.path.realpath(__file__))
+  todayPath = os.path.join(basePath,str(year),"day{:02d}".format(day))
+  templatePath = os.path.join(basePath,"dayX")
+  
+  if not os.path.exists(todayPath):
+    os.makedirs(todayPath)
+
+  print(todayPath)
+
+  for filename in os.listdir(templatePath):
+    if filename == "input.txt":
+      continue
+    source = os.path.join(templatePath, filename)
+    target = os.path.join(todayPath, filename)
+    if not os.path.exists(os.path.join(todayPath,filename)):
+      shutil.copyfile(src=source, dst=target)
+
+  inputFile = os.path.join(todayPath,"input.txt")
+  if not os.path.exists(inputFile):
+    getInput(year,day,inputFile)
+
+  challengeURL = "https://adventofcode.com/{}/day/{}".format(year,day)
+  print ("Challenge URL is:\n\n\t{}\n".format(challengeURL), file=sys.stderr)
 
 def makePath(path):
   os.path.exists(path)
@@ -10,7 +47,6 @@ def makePath(path):
 
 def getInput(year,day,savePath):
   inputURL = "https://adventofcode.com/{}/day/{}/input".format(year,day)
-  print(inputURL)
 
   with open("session.cookie",'r') as sessionFile:
     session = sessionFile.read().replace('\n', '')
@@ -31,43 +67,7 @@ def getInput(year,day,savePath):
         for chunk in result:
             inputFile.write(chunk)
       return 
-    print("Failed to fetch input",result)
-
-def main():
-    now = datetime.datetime.now()
-
-    if now.month != 12:
-      sys.exit("Wrong month")
-    
-    year = now.year
-    day = now.day
-    
-    if day>25:
-      sys.exit("Come back next year")
-    
-    basePath = os.path.dirname(os.path.realpath(__file__))
-    todayPath = os.path.join(basePath,str(year),"day{:02d}".format(day))
-    templatePath = os.path.join(basePath,"dayX")
-    
-    if not os.path.exists(todayPath):
-      os.makedirs(todayPath)
-
-    for filename in os.listdir(templatePath):
-      if filename == "input.txt":
-        continue
-      source = os.path.join(templatePath, filename)
-      target = os.path.join(todayPath, filename)
-      if not os.path.exists(os.path.join(todayPath,filename)):
-        print("Copying",source,target)
-        shutil.copyfile(src=source, dst=target)
-      else:
-        print("Target already exists",target)
-
-    inputFile = os.path.join(todayPath,"input.txt")
-    if os.path.exists(inputFile):
-      print("Already have input file", inputFile)
-    else:
-      getInput(year,day,inputFile)
+    print("Failed to fetch input",result,file=sys.stderr)
 
 
 if __name__ == "__main__":
