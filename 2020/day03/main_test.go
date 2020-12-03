95aff9055753e133719f01aec60a70c6ffa1db15
Alex Mirski-Fitton <alex@amfweb.co.uk>
2020-12-03 07:48:08 +0000

    Remove slower code path

--- 2020/day03/main_test.go
+++ 2020/day03/main_test.go
@@ -11,33 +11,14 @@ func ExampleMain() {
 	//Part 2: 2698900776
 }
 
-func ExampleMainAlt() {
-	mainAlt()
-	//Output:
-	//Part 1: 262
-	//Part 2: 2698900776
-}
-
-func BenchmarkPart1(b *testing.B) {
+func BenchmarkPart1alt(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		part1()
 	}
 }
 
-func BenchmarkPart2(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		part2()
-	}
-}
-
-func BenchmarkPart1alt(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		part1alt()
-	}
-}
-
 func BenchmarkPart2alt(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		part2alt()
+		part2()
 	}
 }
