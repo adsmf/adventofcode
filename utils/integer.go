fcfec911673b4f4f6b698c99bf7e0ffe649353e8
Alex Mirski-Fitton <alex@amfweb.co.uk>
2020-12-06 21:51:02 +0000

    Add GCD and LCM utilities

---
+++ utils/integer.go
@@ -0,0 +1,30 @@
+package utils
+
+func GreatestCommonDivisorInt(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+func LowestCommonMultipleInt(integers ...int) int {
+	if len(integers) < 2 {
+		return 0
+	}
+	a := integers[0]
+	b := integers[1]
+	integers = integers[2:]
+	g := GreatestCommonDivisorInt(a, b)
+	if g == 0 {
+		return 0
+	}
+	result := a * b / g
+
+	for i := 0; i < len(integers); i++ {
+		result = LowestCommonMultipleInt(result, integers[i])
+	}
+
+	return result
+}
