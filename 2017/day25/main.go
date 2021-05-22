8e352636a39e113e0058c32588dfcc72941b9335
Alex Mirski-Fitton <alex@amfweb.co.uk>
2021-05-22 19:08:32 +0100

    Ugly but functional rule parsing

--- 2017/day25/main.go
+++ 2017/day25/main.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"strings"
+
+	"github.com/adsmf/adventofcode/utils"
+	"gopkg.in/yaml.v3"
 )
 
 func main() {
@@ -12,14 +17,13 @@ func main() {
 }
 
 func part1() int {
-	state := stateA
-	maxSteps := 12994925
-	tape := map[int]int{}
+	state, maxSteps, stateActions := parseSettings("input.txt")
+	tape := map[int]tapeValue{}
 	cursor := 0
 	for i := 0; i < maxSteps; i++ {
 		act := stateActions[state][tape[cursor]]
 		tape[cursor] = act.write
-		cursor += act.move
+		cursor += act.offset
 		state = act.nextState
 	}
 
@@ -32,48 +36,74 @@ func part1() int {
 	return checksum
 }
 
-var stateActions = map[int]stateSettings{
-	stateA: {
-		0: action{1, 1, stateB},
-		1: action{0, -1, stateF},
-	},
-	stateB: {
-		0: action{0, 1, stateC},
-		1: action{0, 1, stateD},
-	},
-	stateC: {
-		0: action{1, -1, stateD},
-		1: action{1, 1, stateE},
-	},
-	stateD: {
-		0: action{0, -1, stateE},
-		1: action{0, -1, stateD},
-	},
-	stateE: {
-		0: action{0, 1, stateA},
-		1: action{1, 1, stateC},
-	},
-	stateF: {
-		0: action{1, -1, stateA},
-		1: action{1, 1, stateA},
-	},
+func parseSettings(filename string) (stateID, int, stateActionMap) {
+	inputBytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	blocks := strings.Split(string(inputBytes), "\n\n")
+	initialBlock, stateBlocks := blocks[0], blocks[1:]
+	initialBlockLines := strings.Split(initialBlock, "\n")
+	startState := stateID(initialBlockLines[0][len("Begin in state ")] - 'A')
+	steps := utils.GetInts(initialBlockLines[1])
+
+	stateActions := stateActionMap{}
+	for _, block := range stateBlocks {
+		blockInfo := map[string]map[string][]string{}
+		yaml.Unmarshal([]byte(block), &blockInfo)
+		for stateLine, stateInfo := range blockInfo {
+			inState := stateID(stateLine[len("In state ")] - 'A')
+			settings := stateSettings{}
+			for ruleLine, ruleActions := range stateInfo {
+				ruleValue := tapeValue(ruleLine[len(ruleLine)-1] - '0')
+				action := action{}
+				for _, actionLine := range ruleActions {
+					words := strings.Split(actionLine, " ")
+					keyWord := words[len(words)-1]
+					switch actionLine[0] {
+					case 'W': // [W]rite the value ...
+						action.write = tapeValue(keyWord[0] - '0')
+					case 'M': // [M]ove ...
+						switch keyWord {
+						case "right.":
+							action.offset = 1
+						case "left.":
+							action.offset = -1
+						default:
+							panic(fmt.Errorf("Don't know how to move '%s'", keyWord))
+						}
+					case 'C': // [C]ontinue with...
+						action.nextState = stateID(keyWord[0] - 'A')
+					}
+				}
+				settings[ruleValue] = action
+			}
+			stateActions[inState] = settings
+		}
+	}
+	return startState, steps[0], stateActions
 }
 
-type stateSettings map[int]action
+type stateActionMap map[stateID]stateSettings
+type stateSettings map[tapeValue]action
 
 type action struct {
-	write     int
-	move      int
-	nextState int
+	write     tapeValue
+	offset    int
+	nextState stateID
 }
 
+type stateID int
+
 const (
-	stateA = iota
+	stateA stateID = iota
 	stateB
 	stateC
 	stateD
 	stateE
 	stateF
 )
+
+type tapeValue = int
 
 var benchmark = false
--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/gdamore/tcell v1.3.0
 	github.com/rivo/tview v0.0.0-20191129065140-82b05c9fb329
 	github.com/stretchr/testify v1.4.0
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
