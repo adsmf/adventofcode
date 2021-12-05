410ff7762108245610b1dad5a33c5918db4603c5
Alex Mirski-Fitton <alex@amfweb.co.uk>
2021-12-05 16:33:05 +0000

    Switch from map to array for performance

--- 2021/day05/main.go
+++ 2021/day05/main.go
@@ -12,13 +12,92 @@ import (
 var input string
 
 func main() {
-	p1, p2 := loadInput()
+	p1, p2 := loadInputArray()
 	if !benchmark {
 		fmt.Printf("Part 1: %d\n", p1)
 		fmt.Printf("Part 2: %d\n", p2)
 	}
 }
 
+// Solution using array rather than map or slice
+func loadInputArray() (int, int) {
+	g1, g2 := arrayGrid{}, arrayGrid{}
+	p1, p2 := 0, 0
+	for _, line := range strings.Split(strings.TrimSpace(input), "\n") {
+		ints := utils.GetInts(line)
+		x1, y1 := ints[0], ints[1]
+		x2, y2 := ints[2], ints[3]
+		dX, dY := x2-x1, y2-y1
+
+		count := max(abs(dX), abs(dY))
+		dX /= count
+		dY /= count
+		incG1 := dX == 0 || dY == 0
+
+		for i, x, y := 0, x1, y1; i <= count; i, x, y = i+1, x+dX, y+dY {
+			pos := x*1000 + y
+			if incG1 {
+				switch g1[pos] {
+				case 0:
+					g1[pos] = 1
+				case 1:
+					g1[pos] = 2
+					p1++
+				}
+			}
+			switch g2[pos] {
+			case 0:
+				g2[pos] = 1
+			case 1:
+				g2[pos] = 2
+				p2++
+			}
+		}
+	}
+
+	return p1, p2
+}
+
+// Solution using slice rather than map
+func loadInputSlice() (int, int) {
+	g1, g2 := make(sliceGrid, 1000*1000), make(sliceGrid, 1000*1000)
+	p1, p2 := 0, 0
+	for _, line := range strings.Split(strings.TrimSpace(input), "\n") {
+		ints := utils.GetInts(line)
+		x1, y1 := ints[0], ints[1]
+		x2, y2 := ints[2], ints[3]
+		dX, dY := x2-x1, y2-y1
+
+		count := max(abs(dX), abs(dY))
+		dX /= count
+		dY /= count
+		incG1 := dX == 0 || dY == 0
+
+		for i, x, y := 0, x1, y1; i <= count; i, x, y = i+1, x+dX, y+dY {
+			pos := x*1000 + y
+			if incG1 {
+				switch g1[pos] {
+				case 0:
+					g1[pos] = 1
+				case 1:
+					g1[pos] = 2
+					p1++
+				}
+			}
+			switch g2[pos] {
+			case 0:
+				g2[pos] = 1
+			case 1:
+				g2[pos] = 2
+				p2++
+			}
+		}
+	}
+
+	return p1, p2
+}
+
+// Initial solution
 func loadInput() (int, int) {
 	g1, g2 := grid{}, grid{}
 	p1, p2 := grid{}, grid{}
@@ -69,5 +148,8 @@ func max(a, b int) int {
 
 type grid map[pointHash]bool
 type pointHash uint32
+
+type sliceGrid []int
+type arrayGrid [1000 * 1000]int
 
 var benchmark = false
--- 2021/day05/main_test.go
+++ 2021/day05/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math"
 	"testing"
 )
 
@@ -16,4 +17,37 @@ func BenchmarkMain(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		main()
 	}
+}
+
+func BenchmarkGrids(b *testing.B) {
+	b.Run("map", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			loadInput()
+		}
+	})
+	b.Run("slice", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			loadInputSlice()
+		}
+	})
+	b.Run("array", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			loadInputArray()
+		}
+	})
+}
+
+func BenchmarkOptions(b *testing.B) {
+	dX := 109
+	dY := 129
+	b.Run("math", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = int(math.Max(math.Abs(float64(dX)), math.Abs(float64(dY))))
+		}
+	})
+	b.Run("func", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = max(abs(dX), abs(dY))
+		}
+	})
 }
