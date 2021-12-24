d2a2479903c4db8c872e4ac6a79c862e217f3191
Alex Mirski-Fitton <alex@amfweb.co.uk>
2021-12-24 12:47:54 +0000

    Add cache for performance

--- 2021/day24/main.go
+++ 2021/day24/main.go
@@ -21,38 +21,49 @@ func main() {
 }
 
 type solver struct {
-	zDiv  [14]int
-	addX  [14]int
-	addY  [14]int
-	start int
-	step  int
+	zDiv         [14]zType
+	addX         [14]int
+	addY         [14]int
+	start        smallNumber
+	step         smallNumber
+	knownInvalid map[hashNum]bool
 }
 
 func newSolver(input string) solver {
-	s := solver{}
+	s := solver{
+		knownInvalid: make(map[hashNum]bool, 6000),
+	}
 	lines := utils.GetLines(input)
 	for i := 0; i < 14; i++ {
-		s.zDiv[i] = utils.GetInts(lines[i*18+4][6:])[0]
+		s.zDiv[i] = zType(utils.GetInts(lines[i*18+4][6:])[0])
 		s.addX[i] = utils.GetInts(lines[i*18+5][6:])[0]
 		s.addY[i] = utils.GetInts(lines[i*18+15][6:])[0]
 	}
 	return s
 }
 
-func (s solver) run(start, step int) int {
+func (s solver) run(start, step smallNumber) int {
 	s.start = start
 	s.step = step
-	return s.search(0, 0, 0)
+	return int(s.search(0, 0, 0))
 }
 
-func (s solver) search(prefix int, pos int, prevZ int) int {
+type modelNumber int64
+type smallNumber int8
+type zType uint32
+type hashNum uint64
+
+func (s solver) search(prefix modelNumber, pos smallNumber, prevZ zType) modelNumber {
 	if pos == 14 {
 		if prevZ != 0 {
 			return -1
 		}
 		return prefix
 	}
-	max := 1
+	if s.invalid(pos, prevZ) {
+		return -1
+	}
+	max := zType(1)
 	for _, div := range s.zDiv[pos:] {
 		max *= div
 	}
@@ -60,33 +71,44 @@ func (s solver) search(prefix int, pos int, prevZ int) int {
 		return -1
 	}
 	if s.addX[pos] < 0 {
-		digit := s.addX[pos] + prevZ%26
+		digit := smallNumber(s.addX[pos]) + smallNumber(prevZ%26)
 		if digit < 1 || digit > 9 {
 			return -1
 		}
 		z := s.calcZ(pos, prevZ, digit)
-		val := s.search(prefix*10+digit, pos+1, z)
+		val := s.search(prefix*10+modelNumber(digit), pos+1, z)
 		if val > 0 {
 			return val
 		}
 	} else {
 		for i, digit := 0, s.start; i < 9; i, digit = i+1, digit+s.step {
 			z := s.calcZ(pos, prevZ, digit)
-			val := s.search(prefix*10+digit, pos+1, z)
+			val := s.search(prefix*10+modelNumber(digit), pos+1, z)
 			if val > 0 {
 				return val
 			}
 		}
 	}
+	s.markInvalid(pos, prevZ)
 	return -1
 }
 
-func (s solver) calcZ(pos int, z int, w int) int {
-	x := s.addX[pos] + z%26
+func (s solver) invalid(pos smallNumber, z zType) bool {
+	hash := hashNum(z) + (hashNum(pos) << 32)
+	return s.knownInvalid[hash]
+}
+
+func (s solver) markInvalid(pos smallNumber, z zType) {
+	hash := hashNum(z) + (hashNum(pos) << 32)
+	s.knownInvalid[hash] = true
+}
+
+func (s solver) calcZ(pos smallNumber, z zType, w smallNumber) zType {
+	x := smallNumber(s.addX[pos]) + smallNumber(z%26)
 	z /= s.zDiv[pos]
 	if x != w {
 		z *= 26
-		z += w + s.addY[pos]
+		z += zType(w) + zType(s.addY[pos])
 	}
 	return z
 }
