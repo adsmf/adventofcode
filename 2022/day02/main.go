e215ac9295f69008f853c8155432bb2de1312013
Alex Mirski-Fitton <alex@amfweb.co.uk>
2022-12-02 08:26:24 +0000

    Simpler calculation

--- 2022/day02/main.go
+++ 2022/day02/main.go
@@ -18,40 +18,35 @@ func main() {
 
 func solve() (int, int) {
 	p1score, p2score := 0, 0
+	p1lookup, p2lookup := calcLookups()
 	for pos := 0; pos < len(input); pos += 4 {
 		opSym := symbol(input[pos] - 'A')
 		mySym := symbol(input[pos+2] - 'X')
-
-		winScore, choiceScore := scoreRound(opSym, mySym)
-		p1score += winScore + choiceScore
-
-		p2target := int(mySym * 3)
-		if winScore == p2target {
-			p2score += winScore + choiceScore
-		} else {
-			for choice := symbolRock; choice <= symbolScissors; choice++ {
-				winScore, choiceScore := scoreRound(opSym, choice)
-				if p2target == winScore {
-					p2score += winScore + choiceScore
-					break
-				}
-			}
-		}
+		p1score += p1lookup[inState(opSym, mySym)]
+		p2score += p2lookup[inState(opSym, mySym)]
 	}
 	return p1score, p2score
 }
 
-func scoreRound(op, me symbol) (int, int) {
-	choiceScore := int(me) + 1
-	switch {
-	case op == me:
-		return 3, choiceScore
-	case (op+1)%3 == me:
-		return 6, choiceScore
-	default:
-		return 0, choiceScore
+func calcLookups() (scoreLookup, scoreLookup) {
+	p1 := make(scoreLookup, 1<<4)
+	p2 := make(scoreLookup, 1<<4)
+
+	for op := symbolRock; op <= symbolScissors; op++ {
+		for me := symbolRock; me <= symbolScissors; me++ {
+			winScore, choiceScore := scoreRound(op, me)
+			p1[inState(op, me)] = winScore + choiceScore
+			p2[inState(op, symbol(winScore)/3)] = int(me) + 1 + winScore
+		}
 	}
+	return p1, p2
 }
+
+type scoreLookup []int
+
+func inState(op symbol, me symbol) byte { return byte(op)<<2 + byte(me) }
+
+func scoreRound(op, me symbol) (int, int) { return 3 * int((4+me-op)%3), int(me) + 1 }
 
 type symbol int
 
