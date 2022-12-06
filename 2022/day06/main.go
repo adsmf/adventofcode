0227ddff2b379a67fcef7242986b839525bbb9ed
Alex Mirski-Fitton <alex@amfweb.co.uk>
2022-12-06 05:24:05 +0000

    Performance improvements

--- 2022/day06/main.go
+++ 2022/day06/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"math/bits"
 )
 
 //go:embed input.txt
@@ -17,29 +18,23 @@ func main() {
 }
 
 func solve() (int, int) {
-	p1, p2 := 0, 0
 	const len1, len2 = 4, 14
-	for i := len1; i < len(input); i++ {
-		last := make(map[byte]int, len1)
-		for _, ch := range input[i-len1 : i] {
-			last[ch]++
-		}
-		if len(last) == len1 {
-			p1 = i
-			break
-		}
-	}
-	for i := len2 - len1 + p1; i < len(input); i++ {
-		last := make(map[byte]int, len2)
-		for _, ch := range input[i-len2 : i] {
-			last[ch]++
-		}
-		if len(last) == len2 {
-			p2 = i
-			break
-		}
-	}
+	p1 := findMarker(0, len1)
+	p2 := findMarker(p1-len1, len2)
 	return p1, p2
+}
+
+func findMarker(offset, signalLength int) int {
+	for i := offset + signalLength; i < len(input); i++ {
+		last := uint32(0)
+		for _, ch := range input[i-signalLength : i] {
+			last |= 1 << (ch - 'a')
+		}
+		if bits.OnesCount32(last) == signalLength {
+			return i
+		}
+	}
+	panic("not found")
 }
 
 var benchmark = false
