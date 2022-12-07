3195ecb6a60ee76df2b8d9877e16b1e707873214
Alex Mirski-Fitton <alex@amfweb.co.uk>
2022-12-07 06:26:31 +0000

    Cleanup and performance improvements

--- 2022/day07/main.go
+++ 2022/day07/main.go
@@ -11,85 +11,80 @@ import (
 var input string
 
 func main() {
-	p1 := part1()
-	p2 := part2()
+	p1, p2 := solve()
 	if !benchmark {
 		fmt.Printf("Part 1: %d\n", p1)
 		fmt.Printf("Part 2: %d\n", p2)
 	}
 }
 
-func part1() int {
-	nodes := buildTree(input)
-	size := 0
-	for _, node := range nodes {
-		if node.size() <= 100000 {
-			size += node.treeSize
-		}
-	}
-	return size
-}
-
-func part2() int {
-	minDir := utils.MaxInt
-	nodes := buildTree(input)
+func solve() (int, int) {
+	nodes := make([]*node, 0, 1000)
+	buildTree(input, &nodes)
+	p1 := 0
+	p2 := utils.MaxInt
 	spaceNeeded := -1 * (70000000 - nodes[0].size() - 30000000)
 	for _, node := range nodes {
+		if node.size() <= 100000 {
+			p1 += node.treeSize
+		}
 		if node.size() >= spaceNeeded {
-			if node.size() < minDir {
-				minDir = node.size()
+			if node.size() < p2 {
+				p2 = node.size()
 			}
 		}
 	}
-	return minDir
+
+	return p1, p2
 }
 
-func buildTree(input string) []*node {
+func buildTree(in string, allNodes *[]*node) {
 	stack := []*node{}
-	allNodes := []*node{}
 
 	var curNode *node
-	for _, line := range utils.GetLines(input) {
+	for _, line := range utils.GetLines(in) {
 		switch line[0] {
 		case '$': // command: cd or ls
-			if line[2] == 'c' && line[5] == '.' { // Traverse up
+			if line[2] != 'c' {
+				continue
+			}
+			if line[5] == '.' { // Traverse up
 				stack = stack[0 : len(stack)-1]
 				curNode = stack[len(stack)-1]
-			} else if line[2] == 'c' { // cd
-				path := line[5:]
-				if len(stack) < 1 {
-					curNode = &node{
-						name:     "/",
-						children: map[string]*node{},
-						treeSize: -1,
-					}
-					allNodes = append(allNodes, curNode)
-				} else {
-					curNode = stack[len(stack)-1].children[path]
-				}
-				stack = append(stack, curNode)
+				continue
 			}
+			curNode = newNode()
+			*allNodes = append(*allNodes, curNode)
+			if len(stack) > 0 {
+				stack[len(stack)-1].addChild(curNode)
+			}
+			stack = append(stack, curNode)
 		case 'd': // Directory
-			name := line[4:]
-			child := &node{
-				name:     name,
-				children: map[string]*node{},
-				treeSize: -1,
-			}
-			curNode.children[name] = child
-			allNodes = append(allNodes, child)
 		default: // file size
 			curNode.totalFileSize += getInt(line)
 		}
 	}
-	return allNodes
 }
 
+func newNode() *node {
+	return &node{
+		children: make([]*node, 0),
+		treeSize: -1,
+	}
+}
+
+type nodeList []*node
+
 type node struct {
-	name          string
-	children      map[string]*node
+	children      nodeList
+	numChildren   int
 	totalFileSize int
 	treeSize      int
+}
+
+func (n *node) addChild(child *node) {
+	n.numChildren++
+	n.children = append(n.children, child)
 }
 
 func (n *node) size() int {
