9c65e94a2e81c711e7cdee4b2b7839b5ab884191
Alex Mirski-Fitton <alex@amfweb.co.uk>
2022-12-07 07:34:36 +0000

    Fixed size array of nodes

--- 2022/day07/main.go
+++ 2022/day07/main.go
@@ -18,18 +18,19 @@ func main() {
 	}
 }
 
-func solve() (int, int) {
-	nodes := make([]*node, 0, 1000)
-	buildTree(input, &nodes)
-	p1 := 0
-	p2 := utils.MaxInt
-	spaceNeeded := -1 * (70000000 - nodes[0].size() - 30000000)
-	for _, node := range nodes {
+func solve() (fsSize, fsSize) {
+	nodes := nodePool{}
+	numNodes := buildTree(input, &nodes)
+	p1 := fsSize(0)
+	p2 := fsSize(0)
+	spaceNeeded := (nodes[0].size() + 30000000 - 70000000)
+	for i := 0; i < numNodes; i++ {
+		node := nodes[i]
 		if node.size() <= 100000 {
 			p1 += node.treeSize
 		}
 		if node.size() >= spaceNeeded {
-			if node.size() < p2 {
+			if node.size() < p2 || p2 == 0 {
 				p2 = node.size()
 			}
 		}
@@ -38,9 +39,10 @@ func solve() (int, int) {
 	return p1, p2
 }
 
-func buildTree(in string, allNodes *[]*node) {
+func buildTree(in string, nodes *nodePool) int {
 	stack := nodeList{}
 	stackPos := -1
+	nodesAllocated := 0
 
 	var curNode *node
 	for _, line := range utils.GetLines(in) {
@@ -55,39 +57,46 @@ func buildTree(in string, allNodes *[]*node) {
 				continue
 			}
 			stackPos++
-			curNode = &node{treeSize: -1}
-			*allNodes = append(*allNodes, curNode)
+			curNode = &(nodes)[nodesAllocated]
+			nodesAllocated++
 			if stackPos > 0 {
 				stack[stackPos-1].addChild(curNode)
 			}
 			stack[stackPos] = curNode
 		case 'd': // Directory
 		default: // file size
-			curNode.totalFileSize += getInt(line)
+			curNode.addFile(fsSize(getInt(line)))
 		}
 	}
+	return nodesAllocated
 }
 
+type nodePool [180]node
 type nodeList [10]*node
 
+type fsSize uint32
+
 type node struct {
+	totalFileSize fsSize
+	treeSize      fsSize
 	children      nodeList
-	numChildren   int
-	totalFileSize int
-	treeSize      int
+	numChildren   byte
 }
 
 func (n *node) addChild(child *node) {
 	n.children[n.numChildren] = child
 	n.numChildren++
 }
+func (n *node) addFile(size fsSize) {
+	n.totalFileSize += size
+}
 
-func (n *node) size() int {
-	if n.treeSize > -1 {
+func (n *node) size() fsSize {
+	if n.treeSize > 0 {
 		return n.treeSize
 	}
 	size := n.totalFileSize
-	for i := 0; i < n.numChildren; i++ {
+	for i := byte(0); i < n.numChildren; i++ {
 		size += n.children[i].size()
 	}
 	n.treeSize = size
