ad3014dd124388874b4023c91c39e14fc8b7e61c
Alex Mirski-Fitton <alex@amfweb.co.uk>
2022-12-10 06:40:29 +0000

    Performance improvements

--- 2022/day10/main.go
+++ 2022/day10/main.go
@@ -3,105 +3,75 @@ package main
 import (
 	_ "embed"
 	"fmt"
-	"strconv"
-	"strings"
-
-	"github.com/adsmf/adventofcode/utils"
 )
 
 //go:embed input.txt
-var input string
+var input []byte
 
 func main() {
-	listing := parseListing()
-	p1 := part1(listing)
-	p2 := part2(listing)
+	p1, p2 := solve()
 	if !benchmark {
 		fmt.Printf("Part 1: %d\n", p1)
 		fmt.Printf("Part 2:\n%s\n", p2)
 	}
 }
-
-func part1(listing programListing) int {
-	regX := 1
-	signal := 0
-	cycle := 0
+func solve() (int, screenBuffer) {
+	regX, signal, cycle := 1, 0, 0
 	countCycle := 20
-	for i := 0; i < len(listing); i++ {
-		cycles := 1
-		if listing[i].op == opAddx {
-			cycles = 2
+	screen := screenBuffer{40: '\n', 81: '\n', 122: '\n', 163: '\n', 204: '\n', 245: '\n'}
+	screenPos, hPos := 0, 0
+	execCycle := func() {
+		cycle++
+		if cycle == countCycle {
+			signal += cycle * regX
+			countCycle += 40
 		}
-		for j := 0; j < cycles; j++ {
-			cycle++
-			if cycle == countCycle {
-				signal += (cycle) * regX
-				countCycle += 40
-			}
+		hPos++
+		if hPos-regX >= 0 && hPos-regX <= 2 {
+			screen[screenPos] = '#'
+		} else {
+			screen[screenPos] = '.'
 		}
-		regX += listing[i].value
+		screenPos++
+		if hPos == 40 {
+			hPos = 0
+			screenPos++
+		}
 	}
-	return signal
-}
-
-func part2(listing programListing) string {
-	regX := 1
-	cycle := 0
-	sb := strings.Builder{}
-	for i := 0; i < len(listing); i++ {
-		cycles := 1
-		if listing[i].op == opAddx {
-			cycles = 2
+	for i := 0; i < len(input); i++ {
+		switch input[i] {
+		case 'n':
+			i += 4
+			execCycle()
+		case 'a':
+			i += 5
+			var val int
+			val, i = getInt(input, i)
+			execCycle()
+			execCycle()
+			regX += val
 		}
-		for j := 0; j < cycles; j++ {
-			cycle++
-			hPos := cycle % 40
-			if regX >= hPos-2 && regX <= hPos {
-				sb.WriteString("⬜️")
-			} else {
-				sb.WriteString("⬛️")
-			}
-			if hPos == 0 {
-				sb.WriteByte('\n')
-			}
-		}
-		regX += listing[i].value
 	}
-	return sb.String()
+	return signal, screen
 }
 
-type programListing []instruction
-
-type instruction struct {
-	op    opCode
-	value int
-}
-
-type opCode int
-
-const (
-	opNoop = iota
-	opAddx
-)
-
-func parseListing() programListing {
-	listing := programListing{}
-	for _, line := range utils.GetLines(input) {
-		words := strings.Split(line, " ")
-		var inst instruction
-		switch words[0] {
-		case "noop":
-			inst = instruction{op: opNoop, value: 0}
-		case "addx":
-			val, _ := strconv.Atoi(words[1])
-			inst = instruction{
-				op:    opAddx,
-				value: val,
-			}
-		}
-		listing = append(listing, inst)
+func getInt(in []byte, pos int) (int, int) {
+	accumulator := 0
+	negative := false
+	if in[pos] == '-' {
+		negative = true
+		pos++
 	}
-	return listing
+	for ; in[pos] != '\n'; pos++ {
+		accumulator *= 10
+		accumulator += int(in[pos] & 0xf)
+	}
+	if negative {
+		accumulator = -accumulator
+	}
+	return accumulator, pos
 }
+
+type screenBuffer [246]byte
 
 var benchmark = false
