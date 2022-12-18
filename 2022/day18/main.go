01aacd6cea659014864ed0f51bca1dd7781434f8
Alex Mirski-Fitton <alex@amfweb.co.uk>
2022-12-18 06:55:57 +0000

    Heap free

--- 2022/day18/main.go
+++ 2022/day18/main.go
@@ -3,12 +3,10 @@ package main
 import (
 	_ "embed"
 	"fmt"
-
-	"github.com/adsmf/adventofcode/utils"
 )
 
 //go:embed input.txt
-var input string
+var input []byte
 
 func main() {
 	allCubes := loadCubes()
@@ -22,17 +20,23 @@ func main() {
 
 func part1(allCubes cubeSet) int {
 	area := 0
-	faces := [6]point{
-		{1, 0, 0}, {-1, 0, 0},
-		{0, 1, 0}, {0, -1, 0},
-		{0, 0, 1}, {0, 0, -1},
-	}
-	for cube := range allCubes {
-		for _, face := range faces {
-			if allCubes[cube.add(face)] {
-				continue
+	minX, maxX := allCubes.min.x(), allCubes.max.x()
+	minY, maxY := allCubes.min.y(), allCubes.max.y()
+	minZ, maxZ := allCubes.min.z(), allCubes.max.z()
+	for x := minX; x <= maxX; x++ {
+		for y := minY; y <= maxY; y++ {
+			for z := minZ; z <= maxZ; z++ {
+				cube := pointAt(x, y, z)
+				if !allCubes.vals[cube] {
+					continue
+				}
+				for _, face := range faces {
+					if allCubes.vals[cube.add(face)] {
+						continue
+					}
+					area++
+				}
 			}
-			area++
 		}
 	}
 	return area
@@ -40,21 +44,20 @@ func part1(allCubes cubeSet) int {
 
 func part2(allCubes cubeSet) int {
 	area := 0
-	min, max := point{999, 999, 999}, point{}
-	for c := range allCubes {
-		min = min.minBound(c)
-		max = max.maxBound(c)
-	}
-	min = point{min.x - 1, min.y - 1, min.z - 1}
-	max = point{max.x + 1, max.y + 1, max.z + 1}
-	explored := map[point]bool{min: true}
-	toExplore := []point{min}
-	nextExplore := []point{}
-	for len(toExplore) > 0 {
-		nextExplore = nextExplore[0:0]
-		for _, exploreFrom := range toExplore {
-			for _, adj := range exploreFrom.adjacentPoints() {
-				if allCubes[adj] {
+	min := allCubes.min.add(pointAt(-1, -1, -1))
+	max := allCubes.max.add(pointAt(1, 1, 1))
+	explored := [1 << 15]bool{}
+	explored[min] = true
+	const exploreSize = 2500
+	toExplore := [exploreSize]point{min}
+	nextExplore := [exploreSize]point{}
+	numExplore := 1
+	for numExplore > 0 {
+		numNext := 0
+		for i := 0; i < numExplore; i++ {
+			exploreFrom := toExplore[i]
+			for _, adj := range exploreFrom.adjacent() {
+				if allCubes.vals[adj] {
 					area++
 					continue
 				}
@@ -65,28 +68,50 @@ func part2(allCubes cubeSet) int {
 				if min != min.minBound(adj) || max != max.maxBound(adj) {
 					continue
 				}
-				nextExplore = append(nextExplore, adj)
+				nextExplore[numNext] = adj
+				numNext++
 			}
 		}
 		toExplore, nextExplore = nextExplore, toExplore
+		numExplore = numNext
 	}
 	return area
 }
 
-type cubeSet map[point]bool
+type cubeSet struct {
+	vals     [1 << 15]bool
+	min, max point
+}
 
-type point struct{ x, y, z int }
+const pointOffset = 2
 
-func (p point) minBound(o point) point { return point{min(p.x, o.x), min(p.y, o.y), min(p.z, o.z)} }
-func (p point) maxBound(o point) point { return point{max(p.x, o.x), max(p.y, o.y), max(p.z, o.z)} }
-func (p point) add(o point) point      { return point{p.x + o.x, p.y + o.y, p.z + o.z} }
-func (p point) adjacentPoints() []point {
-	adjacent := make([]point, 6)
-	faces := [6]point{
-		{1, 0, 0}, {-1, 0, 0},
-		{0, 1, 0}, {0, -1, 0},
-		{0, 0, 1}, {0, 0, -1},
-	}
+func pointAt(x, y, z int) point {
+	return point(
+		(((x + pointOffset) & 0x1f) << 10) |
+			(((y + pointOffset) & 0x1f) << 5) |
+			((z + pointOffset) & 0x1f),
+	)
+}
+
+type point int16
+
+func (p point) String() string {
+	return fmt.Sprintf("(%d,%d,%d)", p.x(), p.y(), p.z())
+}
+
+func (p point) x() int { return int((p>>10)&0x1f - pointOffset) }
+func (p point) y() int { return int((p>>5)&0x1f - pointOffset) }
+func (p point) z() int { return int((p)&0x1f - pointOffset) }
+
+func (p point) minBound(o point) point {
+	return pointAt(min(p.x(), o.x()), min(p.y(), o.y()), min(p.z(), o.z()))
+}
+func (p point) maxBound(o point) point {
+	return pointAt(max(p.x(), o.x()), max(p.y(), o.y()), max(p.z(), o.z()))
+}
+func (p point) add(o point) point { return pointAt(p.x()+o.x(), p.y()+o.y(), p.z()+o.z()) }
+func (p point) adjacent() [6]point {
+	adjacent := [6]point{}
 	for i, face := range faces {
 		adjacent[i] = p.add(face)
 	}
@@ -108,11 +133,33 @@ func max(a, b int) int {
 
 func loadCubes() cubeSet {
 	cubes := cubeSet{}
-	for _, line := range utils.GetLines(input) {
-		points := utils.GetInts(line)
-		cubes[point{points[0], points[1], points[2]}] = true
+	cubes.min = pointAt(255, 255, 255)
+	x, y, z := 0, 0, 0
+	for pos := 0; pos < len(input); pos++ {
+		x, pos = getInt(input, pos)
+		y, pos = getInt(input, pos+1)
+		z, pos = getInt(input, pos+1)
+		cube := pointAt(x, y, z)
+		cubes.min = cubes.min.minBound(cube)
+		cubes.max = cubes.max.maxBound(cube)
+		cubes.vals[cube] = true
 	}
 	return cubes
+}
+
+var faces = [6]point{
+	pointAt(1, 0, 0), pointAt(-1, 0, 0),
+	pointAt(0, 1, 0), pointAt(0, -1, 0),
+	pointAt(0, 0, 1), pointAt(0, 0, -1),
+}
+
+func getInt(in []byte, pos int) (int, int) {
+	accumulator := 0
+	for ; in[pos]&0xf0 == 0x30; pos++ {
+		accumulator *= 10
+		accumulator += int(in[pos] & 0xf)
+	}
+	return accumulator, pos
 }
 
 var benchmark = false
--- 2022/day18/main_test.go
+++ 2022/day18/main_test.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func ExampleMain() {
@@ -9,6 +12,23 @@ func ExampleMain() {
 	//Output:
 	//Part 1: 4302
 	//Part 2: 2492
+}
+
+func TestPoint(t *testing.T) {
+	type testPoint [3]int
+	testPoints := []testPoint{
+		{1, 0, 0}, {-1, 0, 0},
+		{0, 1, 0}, {0, -1, 0},
+		{0, 0, 1}, {0, 0, -1},
+	}
+	for _, test := range testPoints {
+		t.Run(fmt.Sprint(test), func(t *testing.T) {
+			pos := pointAt(test[0], test[1], test[2])
+			assert.Equal(t, test[0], pos.x())
+			assert.Equal(t, test[1], pos.y())
+			assert.Equal(t, test[2], pos.z())
+		})
+	}
 }
 
 func BenchmarkMain(b *testing.B) {
