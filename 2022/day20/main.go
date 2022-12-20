29eb37c6779991600cd4d8d081a8be4dca654590
Alex Mirski-Fitton <alex@amfweb.co.uk>
2022-12-20 06:34:31 +0000

    Reduce heap

--- 2022/day20/main.go
+++ 2022/day20/main.go
@@ -4,20 +4,13 @@ import (
 	"container/ring"
 	_ "embed"
 	"fmt"
-
-	"github.com/adsmf/adventofcode/utils"
 )
 
 //go:embed input.txt
-var input string
+var input []byte
 
 func main() {
-	intList := utils.GetInts(input)
-	ints := make([]*int, 0, len(intList))
-	for _, val := range intList {
-		copy := val
-		ints = append(ints, &copy)
-	}
+	ints := getInts(input)
 	p1 := part1(ints)
 	p2 := part2(ints)
 	if !benchmark {
@@ -26,22 +19,21 @@ func main() {
 	}
 }
 
-func part1(ints []*int) int {
+func part1(ints intList) int {
 	r := ring.New(len(ints))
 	var zero *ring.Ring
-	lookup := map[*int]*ring.Ring{}
-	for _, val := range ints {
+	lookup := ringList{}
+	for pos, val := range ints {
 		r.Value = val
 		if *val == 0 {
 			zero = r
 		}
-		lookup[val] = r
+		lookup[pos] = r
 		r = r.Next()
 	}
 
-	for _, val := range ints {
-		r := lookup[val]
-		prev := r.Prev()
+	for pos, val := range ints {
+		prev := lookup[pos].Prev()
 		moveBy := *val % (len(ints) - 1)
 		toMove := prev.Unlink(1)
 		moveTo := prev.Move(moveBy)
@@ -54,26 +46,25 @@ func part1(ints []*int) int {
 	return *val1k + *val2k + *val3k
 }
 
-func part2(ints []*int) int {
+func part2(ints intList) int {
 	decryptionKey := 811589153
 
 	r := ring.New(len(ints))
 	var zero *ring.Ring
-	lookup := map[*int]*ring.Ring{}
-	for _, val := range ints {
+	lookup := ringList{}
+	for pos, val := range ints {
 		*val *= decryptionKey
 		r.Value = val
 		if *val == 0 {
 			zero = r
 		}
-		lookup[val] = r
+		lookup[pos] = r
 		r = r.Next()
 	}
 
 	for i := 0; i < 10; i++ {
-		for _, val := range ints {
-			r := lookup[val]
-			prev := r.Prev()
+		for pos, val := range ints {
+			prev := lookup[pos].Prev()
 			moveBy := *val % (len(ints) - 1)
 			toMove := prev.Unlink(1)
 			moveTo := prev.Move(moveBy)
@@ -85,6 +76,38 @@ func part2(ints []*int) int {
 	val2k := zero.Move(2000).Value.(*int)
 	val3k := zero.Move(3000).Value.(*int)
 	return *val1k + *val2k + *val3k
+}
+
+type intList [5000]*int
+type ringList [5000]*ring.Ring
+
+func getInts(input []byte) intList {
+	ints := intList{}
+
+	for pos, idx := 0, 0; pos < len(input); pos, idx = pos+1, idx+1 {
+		var val int
+		val, pos = getInt(input, pos)
+		ints[idx] = &val
+	}
+
+	return ints
+}
+
+func getInt(in []byte, pos int) (int, int) {
+	accumulator := 0
+	negative := false
+	if in[pos] == '-' {
+		negative = true
+		pos++
+	}
+	for ; in[pos] >= '0' && in[pos] <= '9'; pos++ {
+		accumulator *= 10
+		accumulator += int(in[pos] & 0xf)
+	}
+	if negative {
+		accumulator = -accumulator
+	}
+	return accumulator, pos
 }
 
 var benchmark = false
