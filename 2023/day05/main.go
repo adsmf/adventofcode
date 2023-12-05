f23c011b55465f44852bdb150ab44ccf582d0413
Alex Mirski-Fitton <alex@amfweb.co.uk>
2023-12-05 07:08:37 +0000

    ~40,000,000x performance improvement

--- 2023/day05/main.go
+++ 2023/day05/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"sort"
 	"strings"
 
 	"github.com/adsmf/adventofcode/utils"
@@ -22,7 +23,7 @@ func main() {
 func analyse() (int, int) {
 	var haveSeeds []int
 	currentMap := -1
-	mappings := [7]seedMap{}
+	mappings := seedMapList{}
 	utils.EachLine(input, func(index int, line string) (done bool) {
 		if strings.TrimSpace(line) == "" {
 			return false
@@ -47,64 +48,92 @@ func analyse() (int, int) {
 		}
 		return false
 	})
-	convertSkip := func(seed int) (int, int) {
-		maxSkip := utils.MaxInt
-		var skip int
-		for i := 0; i < len(mappings); i++ {
-			seed, skip = mappings[i].convertSkip(seed)
-			if skip < maxSkip {
-				maxSkip = skip
-			}
-		}
-		return seed, maxSkip
-	}
-	// part 1
+
 	lowestP1 := -1
 	for _, seed := range haveSeeds {
 		for i := 0; i < len(mappings); i++ {
-			seed = mappings[i].convert(seed)
+			seed, _ = mappings[i].convertSkip(seed)
 		}
 		if lowestP1 == -1 || seed < lowestP1 {
 			lowestP1 = seed
 		}
 	}
-	// part 2
+
 	lowestP2 := -1
-	for i := 0; i < len(haveSeeds); i += 2 {
-		for j := 0; j < haveSeeds[i+1]; j++ {
-			skip := 0
-			seed := haveSeeds[i] + j
-			seed, skip = convertSkip(seed)
-			if lowestP2 == -1 || seed < lowestP2 {
-				lowestP2 = seed
-			}
-			if skip > 0 {
-				j += skip - 1
+	flattened := mappings.flatten()
+	sort.Slice(flattened, func(i, j int) bool {
+		return flattened[i].dest < flattened[j].dest
+	})
+	for _, seedRange := range flattened {
+		for i := 0; i < len(haveSeeds); i += 2 {
+			if haveSeeds[i]+haveSeeds[i+1]-1 >= seedRange.src &&
+				haveSeeds[i] <= seedRange.src+seedRange.width-1 {
+				bestSeed := haveSeeds[i]
+				if bestSeed < seedRange.src {
+					bestSeed = seedRange.src
+				}
+				lowestP2, _ = flattened.convertSkip(bestSeed)
+				return lowestP1, lowestP2
 			}
 		}
 	}
 	return lowestP1, lowestP2
 }
 
-type seedMap []seedRange
+type seedMapList [7]seedMaping
 
-func (s seedMap) convert(seed int) int {
-	for _, sr := range s {
-		if seed >= sr.src && seed < (sr.src+sr.width) {
-			return seed - sr.src + sr.dest
+func (s seedMapList) flatten() seedMaping {
+	flat := seedMaping{}
+	for idx := range s {
+		sort.Slice(s[idx], func(i, j int) bool {
+			return s[idx][i].src < s[idx][j].src
+		})
+	}
+	for _, soilRange := range s[0] {
+		for i := 0; i < soilRange.width; {
+			converted, skip := s.convertSkip(soilRange.src + i)
+			newRange := seedRange{
+				src:   soilRange.src + i,
+				dest:  converted,
+				width: skip,
+			}
+			flat = append(flat, newRange)
+			i += skip
+			if skip == 0 {
+				i++
+			}
 		}
 	}
-	return seed
+	return flat
 }
 
-func (s seedMap) convertSkip(seed int) (int, int) {
+func (s seedMapList) convertSkip(seed int) (int, int) {
+	maxSkip := utils.MaxInt
+	var skip int
+	for i := 0; i < len(s); i++ {
+		seed, skip = s[i].convertSkip(seed)
+		if skip < maxSkip {
+			maxSkip = skip
+		}
+	}
+	return seed, maxSkip
+}
+
+type seedMaping []seedRange
+
+func (s seedMaping) convertSkip(seed int) (int, int) {
 	for _, sr := range s {
 		if seed >= sr.src && seed < (sr.src+sr.width) {
 			skip := sr.width - (seed - sr.src)
 			return seed - sr.src + sr.dest, skip
 		}
 	}
-	return seed, 0 // TODO: not zero
+	for i := 0; i < len(s); i++ {
+		if s[i].src > seed {
+			return seed, s[i].src - seed - 1
+		}
+	}
+	return seed, utils.MaxInt
 }
 
 type seedRange struct {
