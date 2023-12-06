19e63a86c347af58c08cc14b9b3fc40c1b802930
Alex Mirski-Fitton <alex@amfweb.co.uk>
2023-12-06 06:03:06 +0000

    Cleanup

--- 2023/day06/main.go
+++ 2023/day06/main.go
@@ -3,88 +3,71 @@ package main
 import (
 	_ "embed"
 	"fmt"
-
-	"github.com/adsmf/adventofcode/utils"
 )
 
 //go:embed input.txt
 var input string
 
 func main() {
-	p1 := part1()
-	p2 := part2()
+	p1, p2 := solve()
 	if !benchmark {
 		fmt.Printf("Part 1: %d\n", p1)
 		fmt.Printf("Part 2: %d\n", p2)
 	}
 }
 
-func part1() int {
-	races := getRaces()
+func solve() (int, int) {
+	races, bigRace := getRaces()
 	multWays := 1
 	for _, race := range races {
 		multWays *= calcRace(race)
 	}
-	return multWays
-}
-
-func part2() int {
-	return calcRace(getBigRace())
+	bigWays := calcRace(bigRace)
+	return multWays, bigWays
 }
 
 func calcRace(race raceInfo) int {
-	winWays := 0
-	for i := 1; i < race.time; i++ {
+	for i := 1; i <= race.time; i++ {
 		remTime := race.time - i
 		dist := remTime * i
 		if dist > race.distance {
-			winWays++
+			lastWin := race.time - i
+			return lastWin - i + 1
 		}
 	}
-	return winWays
+	return -1
 }
 
-func getRaces() []raceInfo {
-	races := []raceInfo{}
-
-	utils.EachLine(input, func(index int, line string) (done bool) {
-		switch index {
-		case 0:
-			utils.EachInteger(line, func(index, time int) (done bool) {
-				races = append(races, raceInfo{time: time})
-				return false
-			})
-		case 1:
-			utils.EachInteger(line, func(index, distance int) (done bool) {
-				races[index].distance = distance
-				return false
-			})
-		}
-		return false
-	})
-	return races
-}
-
-func getBigRace() raceInfo {
-	lineNum := 0
-	time := 0
-	distance := 0
+func getRaces() ([]raceInfo, raceInfo) {
+	smallRaces := []raceInfo{{}}
+	bigRace := raceInfo{}
+	lineNum, curIndex := 0, 0
+	hasDigit := false
 	for _, ch := range input {
 		switch {
 		case ch == '\n':
 			lineNum++
-		case ch >= '0' && ch <= '9':
+			curIndex = 0
+			hasDigit = false
+		case ch == ' ' && hasDigit:
+			hasDigit = false
+			curIndex++
 			if lineNum == 0 {
-				time = 10*time + int(ch-'0')
+				smallRaces = append(smallRaces, raceInfo{})
+			}
+		case ch >= '0' && ch <= '9':
+			hasDigit = true
+			smallRace := &smallRaces[curIndex]
+			if lineNum == 0 {
+				bigRace.time = 10*bigRace.time + int(ch-'0')
+				smallRace.time = 10*smallRace.time + int(ch-'0')
 			} else {
-				distance = 10*distance + int(ch-'0')
+				bigRace.distance = 10*bigRace.distance + int(ch-'0')
+				smallRace.distance = 10*smallRace.distance + int(ch-'0')
 			}
 		}
 	}
-	return raceInfo{
-		time:     time,
-		distance: distance,
-	}
+	return smallRaces, bigRace
 }
 
 type raceInfo struct {
