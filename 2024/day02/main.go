b113d69575321c1f19c83ec43aad5d3575245949
Alex Mirski-Fitton <alex@amfweb.co.uk>
2024-12-02 20:44:35 +0000

    Make day 2 heap free

--- 2024/day02/main.go
+++ 2024/day02/main.go
@@ -21,34 +21,38 @@ func main() {
 
 func solve() (int, int) {
 	p1, p2 := 0, 0
+	readings := make([]int, 0, 10)
 	utils.EachLine(input, func(index int, line string) (done bool) {
-		readings := utils.GetInts(line)
-		invalid := 0
+		readings = readings[0:0]
+		utils.EachInteger(line, func(index, value int) (done bool) {
+			readings = append(readings, value)
+			return false
+		})
+		invalidSkipped := false
 		validP1, validP2 := true, true
 		stepLow, stepHigh := 1, 3
 		if readings[len(readings)-1] < readings[0] {
 			stepLow, stepHigh = -3, -1
 		}
+		safeStep := func(i, j int) bool {
+			return readings[j] >= readings[i]+stepLow && readings[j] <= readings[i]+stepHigh
+		}
 		for i := 1; i < len(readings); i++ {
-			low, high := readings[i-1]+stepLow, readings[i-1]+stepHigh
-			if readings[i] < low ||
-				readings[i] > high {
+			if !safeStep(i-1, i) {
 				validP1 = false
-				invalid++
-				if invalid > 1 {
+				if invalidSkipped {
 					validP2 = false
 					break
 				}
+				invalidSkipped = true
 				if i == len(readings)-1 {
 					break
 				}
 				i++
-				if i == 2 && (readings[i] >= readings[i-1]+stepLow &&
-					readings[i] <= readings[i-1]+stepHigh) {
+				if i == 2 && safeStep(i-1, i) {
 					continue
 				}
-				if readings[i] < low ||
-					readings[i] > high {
+				if !safeStep(i-2, i) {
 					validP2 = false
 					break
 				}
