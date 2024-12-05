91e649934c7e7aafdbcbb046dc810982bc9a9827
Alex Mirski-Fitton <alex@amfweb.co.uk>
2024-12-05 07:16:01 +0000

    Make day 5 heapless

--- 2024/day05/main.go
+++ 2024/day05/main.go
@@ -4,9 +4,6 @@ import (
 	_ "embed"
 	"fmt"
 	"slices"
-	"strconv"
-
-	"github.com/adsmf/adventofcode/utils"
 )
 
 //go:embed input.txt
@@ -22,45 +19,44 @@ func main() {
 
 func solve() (int, int) {
 	p1, p2 := 0, 0
-	order := [100][]int{}
-	utils.EachLine(input, func(index int, line string) (done bool) {
-		if line == "" {
-			return false
+	order := [100][100]bool{}
+
+	orderSec := true
+	pages := make([]byte, 0, 25)
+	for pos := 0; pos < len(input)-1; pos++ {
+		if input[pos] == '\n' {
+			orderSec = false
+			continue
 		}
-		if line[2] == '|' {
-			x, _ := strconv.Atoi(line[0:2])
-			y, _ := strconv.Atoi(line[3:5])
-			order[x] = append(order[x], y)
-			return false
+		if orderSec {
+			x := (input[pos]-'0')*10 + (input[pos+1] - '0')
+			y := (input[pos+3]-'0')*10 + (input[pos+4] - '0')
+			pos += 5
+			order[x][y] = true
+			continue
 		}
-		pages := utils.GetInts(line)
-		pagesUsed := [100]bool{}
-		valid := true
-	check:
-		for _, page := range pages {
-			for _, preReq := range order[page] {
-				if pagesUsed[preReq] {
-					valid = false
-					break check
-				}
+
+		for pages = pages[0:0]; pos < len(input); pos++ {
+			page := (input[pos]-'0')*10 + (input[pos+1] - '0')
+			pages = append(pages, page)
+			pos += 2
+			if pos >= len(input) || input[pos] == '\n' {
+				break
 			}
-			pagesUsed[page] = true
 		}
-		if valid {
-			p1 += pages[len(pages)/2]
-			return false
-		}
-		slices.SortFunc(pages, func(a, b int) int {
-			for _, preReq := range order[a] {
-				if preReq == b {
-					return -1
-				}
+		sortFn := func(a, b byte) int {
+			if order[a][b] {
+				return -1
 			}
 			return 0
-		})
-		p2 += pages[len(pages)/2]
-		return false
-	})
+		}
+		if slices.IsSortedFunc(pages, sortFn) {
+			p1 += int(pages[len(pages)/2])
+			continue
+		}
+		slices.SortFunc(pages, sortFn)
+		p2 += int(pages[len(pages)/2])
+	}
 	return p1, p2
 }
 
