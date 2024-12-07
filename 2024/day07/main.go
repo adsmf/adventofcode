2930f7272c2f1fd26be6caddfa732202644b62a0
Alex Mirski-Fitton <alex@amfweb.co.uk>
2024-12-07 08:05:22 +0000

    Make day 7 heapless

--- 2024/day07/main.go
+++ 2024/day07/main.go
@@ -3,9 +3,6 @@ package main
 import (
 	_ "embed"
 	"fmt"
-	"strconv"
-
-	"github.com/adsmf/adventofcode/utils"
 )
 
 //go:embed input.txt
@@ -21,28 +18,52 @@ func main() {
 
 func solve() (int, int) {
 	p1, p2 := 0, 0
-	utils.EachLine(input, func(_ int, line string) (done bool) {
-		vals := utils.GetInts(line)
-		canP1, canP2 := canMake(vals[0], vals[1:]...)
-		if canP1 {
-			p1 += vals[0]
+	vals := make([]int, 0, 15)
+	acc := 0
+	for i := 0; i < len(input); i++ {
+		ch := input[i]
+		switch {
+		case ch == '\n':
+			vals = append(vals, acc)
+			canP1, canP2 := canMake(vals[0], vals[1:]...)
+			if canP1 {
+				p1 += vals[0]
+			}
+			if canP2 {
+				p2 += vals[0]
+			}
+			vals = vals[0:0]
+			acc = 0
+		case ch == ':':
+			i++
+			fallthrough
+		case ch == ' ':
+			vals = append(vals, acc)
+			acc = 0
+		default:
+			acc *= 10
+			acc += int(ch - '0')
 		}
-		if canP2 {
-			p2 += vals[0]
-		}
-		return false
-	})
+	}
 	return p1, p2
 }
 
+const searchBuffer = 200_000
+
+var open = make([]search, 0, searchBuffer)
+var next = make([]search, 0, searchBuffer)
+
+type search struct {
+	pos    int
+	total  int
+	concat bool
+}
+
 func canMake(target int, operands ...int) (bool, bool) {
-	type search struct {
-		pos    int
-		total  int
-		concat bool
-	}
 	validP2 := false
-	open, next := []search{{1, operands[0], false}}, []search{}
+	open = open[0:0]
+	next = next[0:0]
+	open = append(open, search{1, operands[0], false})
 	for len(open) > 0 {
 		for _, cur := range open {
 			if cur.pos == len(operands) {
@@ -60,12 +81,21 @@ func canMake(target int, operands ...int) (bool, bool) {
 			}
 			next = append(next, search{cur.pos + 1, cur.total * operands[cur.pos], cur.concat})
 			next = append(next, search{cur.pos + 1, cur.total + operands[cur.pos], cur.concat})
-			concat, _ := strconv.Atoi(fmt.Sprintf("%d%d", cur.total, operands[cur.pos]))
+			concat := concatInts(cur.total, operands[cur.pos])
 			next = append(next, search{cur.pos + 1, concat, true})
 		}
 		open, next = next, open[0:0]
 	}
 	return false, validP2
+}
+
+func concatInts(a, b int) int {
+	bOff := 10
+	a *= 10
+	for ; bOff <= b; bOff *= 10 {
+		a *= 10
+	}
+	return a + b
 }
 
 var benchmark = false
