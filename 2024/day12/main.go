301582bfad8fcb29ad5b69bf1dccf0af7c69eaaf
Alex Mirski-Fitton <alex@amfweb.co.uk>
2024-12-13 07:09:12 +0000

    Make day 12 heap free

--- 2024/day12/main.go
+++ 2024/day12/main.go
@@ -30,7 +30,7 @@ func solve() (int, int) {
 	visited := make([]bool, gridAlloc)
 	open := make([]search, 0, 20)
 	next := make([]search, 0, 20)
-	perimiterPoints := make(map[sideInfo]bool, 200)
+	// perimiterPoints := make(map[sideInfo]bool, 200)
 	curRegion := make([]point, 0, 300)
 	x, y := 0, 0
 	for _, ch := range input {
@@ -49,46 +49,44 @@ func solve() (int, int) {
 		curRegion = curRegion[0:0]
 		curRegion = append(curRegion, pos)
 		curPerimiterLen := 0
-		clear(perimiterPoints)
+		numSides := 0
+		// clear(perimiterPoints)
 		for len(open) > 0 {
 			for _, cur := range open {
+				outside := [8]bool{}
 				for dir, n := range cur.pos.neighbours() {
 					nVal := g.valAt(n)
-					if nVal == byte(ch) {
-						if visited[g.index(n)] {
-							continue
+					if dir < 4 {
+						if nVal == byte(ch) {
+							if visited[g.index(n)] {
+								continue
+							}
+							visited[g.index(n)] = true
+							curRegion = append(curRegion, n)
+							next = append(next, search{n, byte(ch)})
+						} else {
+							curPerimiterLen++
 						}
-						visited[g.index(n)] = true
-						curRegion = append(curRegion, n)
-						next = append(next, search{n, byte(ch)})
-					} else {
-						curPerimiterLen++
-						perimiterPoints[sideInfo{n, dir}] = true
+					}
+					if nVal != byte(ch) {
+						outside[dir] = true
+					}
+				}
+				// Convex corner
+				for i := range 4 {
+					if outside[i] && outside[(i+1)&3] {
+						numSides++
+					}
+				}
+				// Concave corner
+				for i := 4; i < 8; i++ {
+					if outside[i] && !outside[(i-4)&3] && !outside[(i-3)&3] {
+						numSides++
 					}
 				}
 			}
 			open, next = next, open[0:0]
 		}
-		numSides := 0
-		for p := range perimiterPoints {
-			numSides++
-			open := make([]sideInfo, 0, 4)
-			open = append(open, p)
-			next := make([]sideInfo, 0, 4)
-			for len(open) > 0 {
-				for _, cur := range open {
-					for _, n := range cur.pos.neighbours() {
-						side := sideInfo{n, cur.edge}
-						if perimiterPoints[side] {
-							delete(perimiterPoints, side)
-							next = append(next, side)
-						}
-					}
-				}
-				open, next = next, open[0:0]
-			}
-		}
-
 		p1 += len(curRegion) * curPerimiterLen
 		p2 += len(curRegion) * (numSides)
 		x++
@@ -122,12 +120,18 @@ func (g grid) index(p point) int {
 
 type point struct{ x, y int }
 
-func (p point) neighbours() [4]point {
-	return [4]point{
+func (p point) neighbours() [8]point {
+	return [8]point{
+		// Adjacent
 		{p.x - 1, p.y},
 		{p.x, p.y - 1},
 		{p.x + 1, p.y},
 		{p.x, p.y + 1},
+		// Diagonals
+		{p.x - 1, p.y - 1},
+		{p.x + 1, p.y - 1},
+		{p.x + 1, p.y + 1},
+		{p.x - 1, p.y + 1},
 	}
 }
 
