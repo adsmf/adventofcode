f274fde60bb0b24c9d2ff4975807b0f70867778f
Alex Mirski-Fitton <alex@amfweb.co.uk>
2025-12-01 11:05:00 +0000

    2025 day 1

---
+++ 2025/day01/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"strconv"
+
+	"github.com/adsmf/adventofcode/utils"
+)
+
+//go:embed input.txt
+var input string
+
+func main() {
+	p1, p2 := solve()
+	if !benchmark {
+		fmt.Printf("Part 1: %d\n", p1)
+		fmt.Printf("Part 2: %d\n", p2)
+	}
+}
+
+func solve() (int, int) {
+	const dialSize = 100
+	dialVal := 50
+	p1, p2 := 0, 0
+	lastDir := byte('R')
+	utils.EachLine(input, func(index int, line string) (done bool) {
+		dir := line[0]
+		val, _ := strconv.Atoi(line[1:])
+		rotations := val / dialSize
+		p2 += rotations
+		val -= dialSize * rotations
+		if dir != lastDir {
+			if dialVal > 0 {
+				dialVal = dialSize - dialVal
+			}
+			lastDir = dir
+		}
+		dialVal += val
+		if dialVal >= dialSize {
+			dialVal -= dialSize
+			p2++
+		}
+		if dialVal == 0 {
+			p1++
+		}
+		return false
+	})
+	return p1, p2
+}
+
+var benchmark = false
---
+++ 2025/day01/main_test.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func ExampleMain() {
+	main()
+	//Output:
+	//Part 1: 992
+	//Part 2: 6133
+}
+
+func TestAnswers(t *testing.T) {
+}
+
+func TestPart1Examples(t *testing.T) {
+	type testDef struct {
+		// Test structure here
+	}
+	tests := []testDef{
+		// Test data here
+	}
+	for id, test := range tests {
+		t.Run(fmt.Sprintf("Part1-Test%d", id+1), func(t *testing.T) {
+			t.Logf("Test def:\n %v", test)
+			// Assertions here
+		})
+	}
+}
+
+func TestPart2Examples(t *testing.T) {
+	type testDef struct {
+		// Test structure here
+	}
+	tests := []testDef{
+		// Test data here
+	}
+	for id, test := range tests {
+		t.Run(fmt.Sprintf("Part1-Test%d", id+1), func(t *testing.T) {
+			t.Logf("Test def:\n %v", test)
+			// Assertions here
+		})
+	}
+}
+
+func BenchmarkMain(b *testing.B) {
+	benchmark = true
+	for i := 0; i < b.N; i++ {
+		main()
+	}
+}
+
+// func BenchmarkPart1(b *testing.B) {
+// 	for i := 0; i < b.N; i++ {
+// 		part1()
+// 	}
+// }
+
+// func BenchmarkPart2(b *testing.B) {
+// 	for i := 0; i < b.N; i++ {
+// 		part2()
+// 	}
+// }
