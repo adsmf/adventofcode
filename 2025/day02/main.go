fe5b9def9faeb40a5533c37315f6536c7f95888e
Alex Mirski-Fitton <alex@amfweb.co.uk>
2025-12-02 05:26:55 +0000

    Combine parts

--- 2025/day02/main.go
+++ 2025/day02/main.go
@@ -13,58 +13,34 @@ import (
 var input string
 
 func main() {
-	p1 := part1()
-	p2 := part2()
+	p1, p2 := solve()
 	if !benchmark {
 		fmt.Printf("Part 1: %d\n", p1)
 		fmt.Printf("Part 2: %d\n", p2)
 	}
 }
 
-func part1() int {
+func solve() (int, int) {
 	allIDs := utils.GetInts(input)
-	sum := 0
-	pairs := [][2]int{}
+	p1, p2 := 0, 0
 	for i := 0; i < len(allIDs); i += 2 {
-		pairs = append(pairs, [2]int{allIDs[i], allIDs[i+1]})
-	}
-	for _, pair := range pairs {
-		for id := pair[0]; id <= pair[1]; id++ {
-			idStr := strconv.Itoa(id)
-			if len(idStr)&1 != 0 {
-				continue
-			}
-			half := len(idStr) / 2
-			if idStr[:half] == idStr[half:] {
-				sum += id
-			}
-		}
-	}
-	return sum
-}
-
-func part2() int {
-	allIDs := utils.GetInts(input)
-	sum := 0
-	pairs := [][2]int{}
-	for i := 0; i < len(allIDs); i += 2 {
-		pairs = append(pairs, [2]int{allIDs[i], allIDs[i+1]})
-	}
-	for _, pair := range pairs {
-		for id := pair[0]; id <= pair[1]; id++ {
+		for id := allIDs[i]; id <= allIDs[i+1]; id++ {
 			idStr := strconv.Itoa(id)
 			for reps := 2; reps <= len(idStr); reps++ {
 				if len(idStr)%reps != 0 {
 					continue
 				}
 				if idStr == strings.Repeat(idStr[:len(idStr)/reps], reps) {
-					sum += id
+					if reps == 2 {
+						p1 += id
+					}
+					p2 += id
 					break
 				}
 			}
 		}
 	}
-	return sum
+	return p1, p2
 }
 
 var benchmark = false
