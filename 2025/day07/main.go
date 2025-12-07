9443d7c9071cdb28be48f6b7c02c0c2074d65d4e
Alex Mirski-Fitton <alex@amfweb.co.uk>
2025-12-07 06:02:25 +0000

    Combine parsing and processing

--- 2025/day07/main.go
+++ 2025/day07/main.go
@@ -3,103 +3,56 @@ package main
 import (
 	_ "embed"
 	"fmt"
-	"slices"
-
-	"github.com/adsmf/adventofcode/utils"
 )
 
 //go:embed input.txt
 var input string
 
 func main() {
-	p1 := part1()
-	p2 := part2()
+	p1, p2 := solve()
 	if !benchmark {
 		fmt.Printf("Part 1: %d\n", p1)
 		fmt.Printf("Part 2: %d\n", p2)
 	}
 }
 
-func part1() int {
-	g := grid{}
-	g.load()
-	beams, next := []int{g.startCol}, []int{}
-	splits := 0
-	for row := 0; row < g.h; row++ {
-		for _, beam := range beams {
-			beamPos := point{beam, row}
-			if g.tiles[beamPos] == tileSplitter {
-				next = append(next, beam-1, beam+1)
-				splits++
-			} else {
-				next = append(next, beam)
-			}
+func solve() (int, int) {
+	width := 0
+	for i, ch := range input {
+		if ch == '\n' {
+			width = i
+			break
 		}
-		next = slices.Compact(next)
-		next, beams = beams[0:0], next
 	}
-	return splits
-}
-
-func part2() int {
-	g := grid{}
-	g.load()
-	beams, next := make([]int, g.w), make([]int, g.w)
-	beams[g.startCol] = 1
+	beams, next := make([]int, width), make([]int, width)
 	splits := 0
-	for row := 0; row < g.h; row++ {
-		for beam, count := range beams {
-			beamPos := point{beam, row}
-			if g.tiles[beamPos] == tileSplitter {
-				next[beam-1] += count
-				next[beam+1] += count
+	x := 0
+	for _, ch := range input {
+		switch ch {
+		case '\n':
+			x = 0
+			clear(beams)
+			next, beams = beams, next
+		case '^':
+			next[x-1] += beams[x]
+			next[x+1] += beams[x]
+			if beams[x] > 0 {
 				splits++
-			} else {
-				next[beam] += count
 			}
+			x++
+		case '.':
+			next[x] += beams[x]
+			x++
+		case 'S':
+			next[x] = 1
+			x++
 		}
-		clear(beams)
-		next, beams = beams, next
 	}
 	count := 0
 	for _, beam := range beams {
 		count += beam
 	}
-	return count
+	return splits, count
 }
-
-type grid struct {
-	tiles    map[point]tileType
-	h, w     int
-	startCol int
-}
-
-func (g *grid) load() {
-	g.tiles = map[point]tileType{}
-	utils.EachLine(input, func(y int, line string) (done bool) {
-		g.w = len(line)
-		g.h = y
-		for x, ch := range line {
-			pos := point{x, y}
-			switch ch {
-			case '^':
-				g.tiles[pos] = tileSplitter
-			case 'S':
-				g.startCol = x
-			}
-		}
-		return
-	})
-}
-
-type tileType byte
-
-const (
-	tileEmpty tileType = iota
-	tileSplitter
-	tileBeam
-)
-
-type point struct{ x, y int }
 
 var benchmark = false
